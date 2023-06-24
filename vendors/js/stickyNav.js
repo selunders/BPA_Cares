3c8551292dfa177465d0ee0c4f4da8ed433e828d
Seth <32936504+selunders@users.noreply.github.com>
2023-06-24 10:39:06 -0700

    Initial Site Upload

---
+++ vendors/js/stickyNav.js
@@ -0,0 +1,21 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
+
+var shiftWindow = function() { scrollBy(0, 50) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
